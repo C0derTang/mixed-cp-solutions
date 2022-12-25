64cc74e67b9b353dfa435fce95cfdf3f7f687bfc
C0derTang <67521455+C0derTang@users.noreply.github.com>
2022-12-24 21:16:49 -0800

    clean code

--- USACO/USACO 2020 US Open Bronze Social Distancing I.py
+++ USACO/USACO 2020 US Open Bronze Social Distancing I.py
@@ -15,37 +15,6 @@ for i in range(n):
         gaps.append(i-prev)
         prev = i
 gaps.append(i-prev)
-'''
-#print(gaps)
-
-ans = 0
-if gaps[0] > 0:
-    if gaps[-1] > 0:
-        ans = max(ans, min(gaps))
-    else:
-        ans = max(ans, min(gaps[:len(gaps)-1]))
-elif gaps[-1] > 0:
-    ans = max(ans, min(gaps[1:]))
-
-for i in range(len(gaps)):
-    if gaps[i] // 2 == 0: continue
-    for j in range(i+1, len(gaps)):
-        if gaps[j] // 2 == 0: continue
-        ans = max(ans, gaps[i]//2, gaps[j]//2)
-
-print(ans)
-
-'''
-def calc_dist():
-    min_dist = n+1
-    for i in range(n):
-        if not stalls[i]:
-            continue
-        for j in range(i+1, n):
-                if not stalls[j]:
-                    continue
-                min_dist = min(min_dist, j-i)
-    return min_dist
 
 ans = 0
 
@@ -57,7 +26,8 @@ if n <= 100:
             if stalls[j]:
                 continue
             stalls[i], stalls[j] = True, True
-            ans = max(ans, calc_dist())
+            ans = max(ans, 
+                      ())
             stalls[i], stalls[j] = False, False
 else:
     if gaps[0] > 0:
