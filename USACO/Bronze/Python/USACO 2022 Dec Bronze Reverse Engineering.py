1da657b52fedb49b79d15a0fcb00d2a681c105c4
C0derTang <67521455+C0derTang@users.noreply.github.com>
2023-01-06 20:04:02 -0800

    more understandable solution

--- USACO/Bronze/Python/USACO 2022 Dec Bronze Reverse Engineering.py
+++ USACO/Bronze/Python/USACO 2022 Dec Bronze Reverse Engineering.py
@@ -1,55 +1,50 @@
-import io, os, sys, collections, itertools
+import io, os, sys
 
 #input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 input = sys.stdin.readline
 
-
 def solve():
     n, m = map(int, input().split())
-    ips = [None] * m
-    rets = [None] * m
+    inputs = [None] * m
+    outputs = [None] * m
+    tcPassed = [False] * m
     for i in range(m):
         ip, ret = map(str, input().split())
-        ret = int(ret)
-        ips[i] = ip
-        rets[i] = ret
-
-    programs = [i for i in range(m)]
-    
+        inputs[i] = ip
+        outputs[i] = ret
     while True:
-        updated = False
-        for i in range(n):
-            if updated:
-                break
-            offvals = set()
-            offinputs = []
-            onvals = set()
-            oninputs = []
-            for j in programs:
-                if ips[j][i] == '1':
-                    onvals.add(rets[j])
-                    oninputs.append(j)
-                else:
-                    offvals.add(rets[j])
-                    offinputs.append(j)
-            print(offvals, onvals, offinputs, oninputs)
-                    
-            if len(offvals) <= 1 and len(onvals) <= 1:
-                print("OK")
-                return
-            if len(offvals) == 0 or len(onvals) == 0:
-                continue
-            if len(offvals) == 2 and len(onvals) == 2:
-                continue
-            if len(offvals) == 1:
-                updated = True
-                programs = oninputs
-            elif len(onvals) == 1:
-                updated = True
-                programs = offinputs
-        if not updated:
-            print("LIE")
-            return
+        found = False
+        for bit in range(n):
+            if found: break
+            for val in '01':
+                if found: break
+                for output in '01':
+                    if found: break
+                    consistent = True
+                    one = False
+                    for tc in range(m):
+                        if tcPassed[tc]: continue
+
+                        if inputs[tc][bit] == val:
+                            one = True
+                            if outputs[tc] != output:
+                                consistent = False
+                    if consistent and one:
+                        found = True
+                        for tc in range(m):
+                            if tcPassed[tc]: continue
+                            if inputs[tc][bit] == val:
+                                tcPassed[tc] = True
+        if not found: break
+    ok = True
+    for i in range(m):
+        if not tcPassed[i]:
+            ok = False
+            break
+    if ok:
+        print('OK')
+    else:
+        print('LIE')
 
 for _ in range(int(input())):
     input()
