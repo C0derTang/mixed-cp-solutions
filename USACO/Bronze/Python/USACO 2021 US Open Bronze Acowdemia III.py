15a10a74042aae558cbf52bb9c6faf26b48e0bb1
C0derTang <67521455+C0derTang@users.noreply.github.com>
2023-01-26 07:38:08 -0800

    Add files via upload

---
+++ USACO/Bronze/Python/USACO 2021 US Open Bronze Acowdemia III.py
@@ -0,0 +1,29 @@
+import sys, io, os
+
+#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+grid = [list(input().strip()) for _ in range(n)]
+
+poss_pairs = set()
+ans = 0
+for i, row in enumerate(grid):
+    for j, cell in enumerate(row):
+        if cell == 'G':
+            nb = []
+            count = 0
+            for v, h in [[1,0],[-1,0],[0,1],[0,-1]]:
+                if i+v < 0 or i+v >= n or j+h < 0 or j+h >= m:
+                    continue
+                if grid[i+v][j+h] == 'C':
+                    nb.append((i+v, j+h))
+                    count += 1
+                    if count > 2: break
+            if count == 2:
+                poss_pairs.add(tuple(sorted([nb[0], nb[1]])))
+            elif count > 2:
+                ans += 1
+            
+print(len(poss_pairs)+ans)
