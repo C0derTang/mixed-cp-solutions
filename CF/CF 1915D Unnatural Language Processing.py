42bd06c24a76e48c9117e61d1cd15e6c38e9c1fe
C0derTang <67521455+C0derTang@users.noreply.github.com>
2023-12-31 10:58:12 -0800

    Update CF 1915D Unnatural Language Processing.py

--- CF/CF 1915D Unnatural Language Processing.py
+++ CF/CF 1915D Unnatural Language Processing.py
@@ -1,4 +1,29 @@
-# Source: https://usaco.guide/general/io
+import sys, math, collections
 
-a, b, c = map(int, input().split())
-print("The sum of these three numbers is", a + b + c)
+input = sys.stdin.readline
+
+def solve():
+	n = int(input())
+	a = [int(i) for i in input().split()]
+	pd = [0]
+	for i, v in enumerate(a):
+		if i%2 == 0:
+			pd.append(pd[-1]+v)
+		else:
+			pd.append(pd[-1]-v)
+
+	first = {}
+	check = False
+	for i, d in enumerate(pd):
+		if d in first:
+			check = True
+			break
+		first[d] = i
+	if check:
+		print('YES')
+	else:
+		print('NO')
+	
+
+for _ in range(int(input())):
+	solve()
