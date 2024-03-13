4bd71a5f455ba73b2607ebca1836d3c3ce14dd4e
C0derTang <67521455+C0derTang@users.noreply.github.com>
2024-03-12 17:19:16 -0700

    my version

--- USACO/Silver/C++/USACO 2017 US Open Silver Paired Up.cpp
+++ USACO/Silver/C++/USACO 2017 US Open Silver Paired Up.cpp
@@ -1,31 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> pii;
-vector<pii> V;
-int N;
-
 int main()
 {
-  ifstream fin ("pairup.in");
-  ofstream fout ("pairup.out");
-  fin >> N;
-  for (int i=0; i<N; i++) {
-    int x, y;
-    fin >> x >> y;
-    V.push_back(pii(y,x));
-  }
-  sort(V.begin(), V.end());
-  int M = 0, i=0, j=N-1;
-  while (i <= j) {
-    int x = min(V[i].second, V[j].second);
-    if (i==j) x /= 2;
-    M = max(M, V[i].first + V[j].first);
-    V[i].second -= x;
-    V[j].second -= x;
-    if (V[i].second == 0) i++;
-    if (V[j].second == 0) j--;
-  }
-  fout << M << "\n";
-  return 0;
+    int n;
+    cin >> n;
+    
+    map<int, int> cows;
+    for (int i=0; i<n; ++i){
+        int v, k;
+        cin >> v >> k;
+        cows[k] = v;
+    }
+
+    int ans = 0;
+    while (!cows.empty()){
+        int a = cows.begin()->first;
+        int b = cows.rbegin()-> first;
+        ans = max(a + b, ans);
+
+        int ops = min(cows[a], cows[b]);
+        cows[a] -= ops;
+        cows[b] -= ops;
+        if (cows[a] <= 0){
+            cows.erase(a);
+        }
+        if (cows[b] <= 0){
+            cows.erase(b);
+        }
+    }
+    cout << ans;
 }
