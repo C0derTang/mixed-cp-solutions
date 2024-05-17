fb6b8ab48d271f31669b55fd7310eaefb04c2a72
C0derTang <67521455+C0derTang@users.noreply.github.com>
2024-05-16 21:48:24 -0700

    Update USACO 2020 Jan Silver Wormhole Sort.cpp

--- USACO/Silver/C++/USACO 2020 Jan Silver Wormhole Sort.cpp
+++ USACO/Silver/C++/USACO 2020 Jan Silver Wormhole Sort.cpp
@@ -32,37 +32,6 @@ int main() {
 	freopen("wormsort.in", "r", stdin);
 	freopen("wormsort.out", "w", stdout);
 	
-
-	//requirements:
-	// every item has to be sorted
-	// it should matter the order
-	// 3 2 1 4
-	// 1 2 9
-	// 1 3 7
-	// 2 3 10
-	// 2 4 3
-
-	// 3 2 1 4
-	// 1 2 9
-	// 1 3 100
-	// 2 3 10
-	// 2 4 3
-
-	// shitty approach #1 (too expensive)
-	// 1 check what cows are already sorted, keep track of which postions are unsorted
-	// 2 sort wormholes by largest size
-	// 3 go through wormholes largest to smallest
-	// 4 for each wormhole, update the dsu
-	// 5 check if the two cows at the end of the wormholes are connected to the cows at the position they should be in
-	//if it is remove from set of unsorted values
-	// break unsorted is emptyonce all unsorted postions are connected to their intended positions
-	// mnlog(n) + mlogm slow af bitch
-
-	//shitty approach #2
-	// bsearch or wormhole width
-	// create new dsu every time and check all connectivities
-
-	
 	int n, m;
 	cin >> n >> m;
 	vector<int> p(n);
