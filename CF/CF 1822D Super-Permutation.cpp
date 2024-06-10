289101a12d1a8e0b6bc2bd6c667bd3afd75c2fcc
C0derTang <67521455+C0derTang@users.noreply.github.com>
2024-06-09 19:33:51 -0700

    Add files via upload

---
+++ CF/CF 1822D Super-Permutation.cpp
@@ -0,0 +1,35 @@
+// CANCER GUESSFORCES
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll long long
+
+bool cbs(pair<int, int> a, pair<int, int> b) {
+    return a.second < b.second;
+}
+
+void solve(){
+	int n; cin >> n;
+	if (n%2 && n != 1){
+		cout << "-1\n"; return;
+	}
+
+	for(int i=0; i<n; ++i) cout << (i%2 ? i : n-i ) << ' ';
+	cout << '\n';
+
+}
+
+signed main() {
+	cin.tie(NULL) -> sync_with_stdio(false);
+	int t; cin >> t; // = 1;
+	while(t--){
+		solve();
+	}
+}
