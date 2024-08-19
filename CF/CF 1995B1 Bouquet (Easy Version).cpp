3a07b8ea41bffa5db514ea593cd4a3f326062dee
C0derTang <67521455+C0derTang@users.noreply.github.com>
2024-08-19 06:48:24 -0700

    Add files via upload

---
+++ CF/CF 1995B1 Bouquet (Easy Version).cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+
+#define FOR(i, a, b) for(int i=a; i<b; ++i)
+#define F0R(i, a) for(int i=0; i<a; ++i)
+#define ROF(i, a, b) for(int i=b; i>a-1; --i)
+#define R0F(i, a) for(int i=a; i>-1; --i)
+#define MOD(a, b) ((((a) % (b)) + (b)) % (b))
+
+bool cbs(pii a, pii b) {
+    return a.second < b.second;
+}
+
+
+
+void solve(){
+	int n, m;
+    cin >> n >> m;
+    vi a(n);
+    F0R(i, n) cin >> a[i];
+	sort(a.begin(), a.end());
+
+    int ans=0, curans=0, l=0;
+	F0R(r, n){
+		curans += a[r];
+		while((curans > m || a[r]-a[l] > 1) && l<=r ){
+			curans -= a[l];
+			++l;
+		}
+		ans = max(curans, ans);
+	}
+	cout << ans << '\n';
+}
+
+signed main() {
+	cin.tie(NULL) -> sync_with_stdio(false);
+	int t; cin >> t; // = 1;
+	while(t--){
+		solve();
+	}
+}
