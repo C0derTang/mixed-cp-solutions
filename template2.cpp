97d21a4adce6b751e8df18b9143b0997d041965a
C0derTang <67521455+C0derTang@users.noreply.github.com>
2024-12-21 12:10:26 -0800

    Add files via upload

---
+++ template2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define MOD(a, b) ((((a) % (b)) + (b)) % (b))
+
+bool cbs(pair<int, int> a, pair<int, int> b) {
+    return a.second < b.second;
+}
+
+void solve() {
+    
+}
+
+signed main() {
+    //freopen("input.in", "r", stdin);
+    //freopen("output.out", "w", stdout);
+
+    cin.tie(NULL) -> sync_with_stdio(false);
+    
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
