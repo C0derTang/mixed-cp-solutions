35c0a757419bcfb423dd2a1e3e252877e0de9895
C0derTang <67521455+C0derTang@users.noreply.github.com>
2025-01-05 18:02:15 -0800

    new impl

--- USACO/Gold/C++/USACO 2016 US Open Gold Closing the Farm.cpp
+++ USACO/Gold/C++/USACO 2016 US Open Gold Closing the Farm.cpp
@@ -1,62 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct DSU{
-	vector<int> roots;
-	vector<int> sizes;
-	int largest = 1;
-	void init(int n) {
-		roots = vector<int>(n);
-		for(int i=0; i<n; ++i) {
-			roots[i] = i;
-		}
-		sizes = vector<int>(n, 1);
-	}
-	int find(int x) {return roots[x] == x ? x: (roots[x] = find(roots[x]));}
-	bool unite(int x, int y){
-		int rx = find(x);
-		int ry = find(y);
-		if (rx == ry) return false;
-		if (sizes[rx] < sizes[ry]) swap(rx, ry);
-		sizes[rx] += sizes[ry];
-		largest = max(largest, sizes[rx]);
-		roots[ry] = rx;
-		return true;
-	}
-	bool linked(int x, int y) {return find(x) == find(y);}
+#define ll long long
+#define MOD(a, b) ((((a) % (b)) + (b)) % (b))
+
+bool cbs(pair<int, int> a, pair<int, int> b) {
+    return a.second < b.second;
+}
+
+struct DSU {
+    vector<int> roots;
+    vector<int> sizes;
+    int largest = 1;
+ 
+    void init(int n) {
+        roots = vector<int>(n);
+        sizes = vector<int>(n, 1);
+        for (int i = 0; i < n; ++i) roots[i] = i;
+    }
+ 
+    int find(int x) {
+        return roots[x] == x ? x : (roots[x] = find(roots[x]));
+    }
+ 
+    bool unite(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+        if (rx == ry) return false;
+        if (sizes[rx] < sizes[ry]) swap(rx, ry);
+        sizes[rx] += sizes[ry];
+        roots[ry] = rx;
+        largest = max(largest, sizes[rx]);
+        return true;
+    }
+
+    bool linked(int x, int y) {return find(x) == find(y);}
+
 };
 
-int main() {
-	freopen("closing.in", "r", stdin);
-	freopen("closing.out", "w", stdout);
-	
-	int n, m;
+
+signed main() {
+    freopen("closing.in", "r", stdin);
+    freopen("closing.out", "w", stdout);
+
+    cin.tie(NULL) -> sync_with_stdio(false);
+    
+    int n, m;
 	cin >> n >> m;
-	DSU farm;
-	farm.init(n);
-	
-	vector<vector<int>> adj(n);
-	while(m--){
-		int a, b;
-		cin >> a >> b;
-		--a; --b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
 
-	vector<int> order(n);
-	for (int i=0; i<n; ++i) cin >> order[i];
-	reverse(order.begin(), order.end());
+    vector<vector<int>> adj(n);
+    for(int i=0; i<m; ++i){
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    vector<int> q(n);
+    for(int i=n-1; i>-1; --i){
+        cin >> q[i];
+        --q[i];
+    }
 
-	set<int> visited;
-	vector<string> ans(n);
-	for (int i=0; i<n; ++i){
-		--order[i];
-		visited.insert(order[i]);
-		for(auto x: adj[order[i]]){
-			if (visited.find(x) != visited.end()) farm.unite(order[i], x);
-		}
-		ans[i] = (farm.largest >= i+1 ? "YES\n" : "NO\n");
-	}
-	for(int i=n-1; i> -1; --i) cout << ans[i];
+    DSU bash;
+    bash.init(n);
+    
+    vector<string> ans(n);
+    vector<bool> open(n);
+    for(int i=0; i<n; ++i){
+        open[q[i]] = true;
+        for(auto x: adj[q[i]]){
+            if (open[x]) bash.unite(q[i], x);
+        }
+        ans[i] = (bash.largest == i+1) ? "YES\n":"NO\n";
+    }
+	for(int i=n-1; i>-1; --i) cout << ans[i];
 }
